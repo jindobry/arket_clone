2811991c42603b1a83699a556c0e72ddca2eeb81
jinheekim <jinheeee72@gmail.com>
2020-10-30 17:50:01 +0900

    완료

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
 export const API = 'http://10.58.6.254:8000';
-<<<<<<< HEAD
-=======
-export const baekAPI = 'http://10.58.6.254:8000';
->>>>>>> 94b9d80... Color Wheel Filter Finish
-export const reviewsTestToken =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvanVAbmF2ZXIuY29tIn0.oXcsGDpGHWkSYpLNOxQA_bU4lRJZukLqa8y_ffX9YIE';
+export const reviewsTestToken = '0';
 export const accessKeyId = '1';
-export const secretAccessKey = 'm2';
+export const secretAccessKey = '2';
 export const kakaoKey = '3';
