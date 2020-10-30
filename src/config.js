b149795906fc74cfa82179c75183a075f44f17a3
jinheekim <jinheeee72@gmail.com>
2020-10-30 17:48:58 +0900

    완료

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ export const baekAPI = 'http://10.58.6.254:8000';
 >>>>>>> 94b9d80... Color Wheel Filter Finish
 export const reviewsTestToken =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvanVAbmF2ZXIuY29tIn0.oXcsGDpGHWkSYpLNOxQA_bU4lRJZukLqa8y_ffX9YIE';
-export const accessKeyId = 'AKIATJ3377PSKQB5HXK5';
-export const secretAccessKey = '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';
-export const kakaoKey = 'ecda3f5f6bbfc2851ca559c4c3174b28';
+export const accessKeyId = '1';
+export const secretAccessKey = 'm2';
+export const kakaoKey = '3';
